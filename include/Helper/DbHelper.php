aa51e13f89fdc26818c758e5c271c197bf675218
Grace Yosheva <151107209+13323021GraceYosheva@users.noreply.github.com>
2024-05-15 13:03:05 +0700

    Update DbHelper.php

--- include/Helper/DbHelper.php
+++ include/Helper/DbHelper.php
@@ -10,6 +10,46 @@ function getAll($koneksi,$table){
     }
     return $result;
 }
+function getAllDataAsConditions($koneksi,$table,$conditions,$select = "*"){
+    $query = mysqli_query($koneksi,"SELECT $select FROM $table WHERE $conditions;");
+    $result = null;
+    if(mysqli_num_rows($query) > 0){
+        $result = $query;
+    }
+    return $result;
+}
+
+function StoreData($koneksi,$table,$data){
+    $result = null;
+    $col = [];
+    $row = [];
+    foreach($data as $key => $value){
+        array_push($col,'`'.$key.'`');
+        array_push($row,"'".$value."'");
+    }
+    $query = "INSERT INTO $table(".implode(",",$col).") VALUES (".implode(",",$row).");";
+    $result = mysqli_query($koneksi,$query);
+    return $result;
+}
+
+function UpdateData($koneksi,$table,$conditions,$data){
+    $result = null;
+    $data_update = [];
+    foreach($data as $key => $value){
+        array_push($data_update,'`'.$key.'`='."'".$value."'");
+    }
+    $query = "UPDATE $table SET ".implode(", ",$data_update)." WHERE ".implode("AND ",$conditions).';';
+    $result = mysqli_query($koneksi,$query);
+    return $result;
+}
+
+function DeleteData($koneksi,$table,$conditions){
+    $result  = null;
+    $query = "DELETE FROM $table WHERE ".implode("AND ",$conditions).";";
+    $result = mysqli_query($koneksi,$query);
+    return $result;
+}
+
 
 function getDataAsConditions($koneksi,$table,$conditions,$select = "*"){
     $query = mysqli_query($koneksi,"SELECT $select FROM $table WHERE $conditions;");
@@ -40,6 +80,28 @@ function getDataCount($koneksi,$table){
 function setFlashData($message,$status = "danger"){
     $_SESSION['flash_data']['message'] = $message;
     $_SESSION['flash_data']['status'] = $status;
+}
+
+function UploadFiles($path = null, $image = null){
+    if($image == null) return null;
+    if($path == null)  return null;
+    $result = null;
+    $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+    $filename = 'UPL_'.date('Y_M_D_h_i_s').'.'.$ext;
+    $file_tmp = $image['tmp_name'];
+    $upload_to = $path.$filename;
+    if (move_uploaded_file($file_tmp, $upload_to)) {
+        $result = $filename;
+    }
+    return $result;
+}
+function getSlug($string = null){
+    if($string == null) return;
+    $string = str_replace(' ', '-', $string);
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+    $result = strtolower($string);
+    return $result;
+    
 }
 
 
